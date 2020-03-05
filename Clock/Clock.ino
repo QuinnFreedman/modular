360cbb80a113b5ee6258a19c5de979a423c4f7f1
Quinn Freedman <quinnfreedman@gmail.com>
2020-03-04 23:30:52 -0800

    clock fully works

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -10,11 +10,20 @@ const int ENCODER_PIN_A = 3;
 const int ENCODER_PIN_B = 4;
 
 const int NUM_OUTPUTS = 8;
+const int OUTPUT_PINS[NUM_OUTPUTS] = {5, 6, 7, 8, 9, 10, 11, 12};
+
+const uint32_t SLEEP_TIMEOUT_MICROS = 8000000;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_I2C_ADDRESS 0x3C // Change to correct address for your screen
+
+#define SS_NONE  0
+#define SS_BLACK 1
+#define SS_PULSE 2
+#define SS_BARS  3
+#define SCREEN_SAVER SS_BLACK 
 
 typedef enum {NAVIGATE, EDIT_FAST, EDIT_SLOW, SLEEP} MenuMode;
 
@@ -23,11 +32,16 @@ typedef struct {
     int values[NUM_OUTPUTS];
     int cursor;
     MenuMode mode;
+    bool newInput;
 } State;
 
 State state;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+uint32_t lastRecordedTime;
+
+uint8_t outputCache[NUM_OUTPUTS] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
 
 void setup() {
     pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -46,31 +60,105 @@ void setup() {
     }
 
     state = {
-        .bpm = 128,
+        .bpm = 70,
         .values = {1, -2, -4, -8, 2, 4, 8, 16},
         .cursor = -1,
-        .mode = NAVIGATE
+        .mode = NAVIGATE,
+        .newInput = true
     };
+    
+    lastRecordedTime = micros();
 }
 
 
 void loop() {
-    /* display.clearDisplay(); */
-    redrawMenu(display, state);
-    delay(100);
+    static uint32_t beatStart = micros();
+    static uint32_t lastInputTime = micros();
+    // Keep track of time
+    static uint32_t numBeats = 0;
+    const uint32_t beatMicros = 60000000 / state.bpm;
+    const uint32_t currentTime = micros();
+    lastRecordedTime = currentTime;
+    uint32_t elapsed = currentTime - beatStart;
+    while (elapsed >= beatMicros) {
+        elapsed -= beatMicros;
+        beatStart = currentTime - elapsed;
+        numBeats++;
+    }
+    float elapsedFraction = ((float) elapsed) / ((float) beatMicros);
+
+    // Check if there has been new knob/button input
+    const bool newInput = state.newInput;
+    if (newInput) {
+        lastInputTime = currentTime;
+        state.newInput = false;
+    }
+
+    // Send signals to output pins
+    for (int i = 0; i < NUM_OUTPUTS; i++) {
+        float elapsed;
+        const int fraction = state.values[i];
+        if (fraction > 0) {
+            // multiple of beat
+            elapsed = getElapsed(fraction, numBeats, elapsedFraction);
+        } else {
+            // fraction of beat
+            const float period = 1.0 / (float) abs(fraction);
+            float remainder = elapsedFraction;
+            while (remainder >= period) {
+                remainder -= period;
+            }
+            elapsed = remainder / period;
+            /* elapsed = elapsedFraction; */
+        }
+        const uint8_t output = elapsed < 0.5 ? HIGH : LOW;
+        if (output != outputCache[i]) {
+            outputCache[i] = output;
+            digitalWrite(OUTPUT_PINS[i], output);
+        }
+    }
+    
+
+
+    // draw screen
+    #if SCREEN_SAVER != SS_NONE
+    bool forceRedrawScreen = false;
+    if (state.mode != SLEEP && currentTime - lastInputTime > SLEEP_TIMEOUT_MICROS) {
+        state.mode = SLEEP;
+        state.cursor = -1;
+        forceRedrawScreen = true;
+    }
+    #endif
+
+    if (state.mode == SLEEP) {
+        float elapsed = getElapsed(1, numBeats, elapsedFraction);
+        drawScreenSaver(display, elapsed, forceRedrawScreen);
+    } else if (newInput) {
+        redrawMenu(display, state);
+    }
+    delay(1);
 }
 
-const uint32_t LONG_PRESS_TIME = 700;
+inline float getElapsed(uint32_t periodLenBeats, uint32_t numBeatsElapsed,
+                       float elapsedInCurrentBeat) {
+    const uint32_t beatsInCurrentPeriod = numBeatsElapsed % periodLenBeats;
+    const float fractionElapsedPrevious = (float) beatsInCurrentPeriod / (float) periodLenBeats;
+    const float fractionElapsed = fractionElapsedPrevious + 
+            (elapsedInCurrentBeat / (float) periodLenBeats);
+    return fractionElapsed;
+}
+
+const uint32_t LONG_PRESS_TIME_MICROS = 500000;
 
 void buttonChangeHandler() {
     static uint32_t pressTime = 0;
     bool buttonPressed = digitalRead(BUTTON_PIN) == LOW;
-    uint32_t now = millis();
+    uint32_t now = lastRecordedTime;
     if (buttonPressed) {
         pressTime = now;
     } else {
         uint32_t holdTime = now - pressTime;
-        if (holdTime >= LONG_PRESS_TIME) {
+        if (holdTime >= LONG_PRESS_TIME_MICROS) {
             onLongPress();
         } else {
             onShortPress();
@@ -86,7 +174,7 @@ void rotaryEncoderHandler() {
     int b = digitalRead(ENCODER_PIN_B);
 
     int currentStateCLK = a;
-    if (currentStateCLK != lastCLK  && currentStateCLK == 1) {
+    if (currentStateCLK != lastCLK && currentStateCLK == 1) {
         int direction;
         if (b != currentStateCLK) {
             direction = -1;
@@ -102,6 +190,7 @@ void rotaryEncoderHandler() {
 }
 
 void onLongPress() {
+    state.newInput = true;
     switch (state.mode) {
     case NAVIGATE: 
         state.mode = EDIT_SLOW;
@@ -113,6 +202,7 @@ void onLongPress() {
 }
 
 void onShortPress() {
+    state.newInput = true;
     switch (state.mode) {
     case NAVIGATE: 
         state.mode = EDIT_FAST;
@@ -124,6 +214,7 @@ void onShortPress() {
 }
 
 void onKnobTurned(int direction, int counter) {
+    state.newInput = true;
     switch (state.mode) {
     case NAVIGATE: {
         state.cursor += direction;
@@ -277,6 +368,49 @@ void redrawMenu(Adafruit_SSD1306 display, State state) {
     }
 
     display.display();
+}
+
+
+inline void drawScreenSaver(Adafruit_SSD1306 display, float elapsedFraction, bool forceRedraw) {
+#if SCREEN_SAVER == SS_BLACK
+    if (forceRedraw) {
+        display.clearDisplay();
+        display.display();
+    }
+#elif SCREEN_SAVER == SS_PULSE
+    static int lastHeight = 0;
+    static int lastWidth = 0;
+    const float fraction = 2 * (elapsedFraction < 0.5 ? elapsedFraction : 1 - elapsedFraction);
+    const int width = fraction * SCREEN_WIDTH;
+    const int height = fraction * SCREEN_HEIGHT;
+    
+    if (!forceRedraw && width == lastWidth && height == lastHeight) {
+        return;
+    }
+
+    lastWidth = width;
+    lastHeight = height;
+
+    display.clearDisplay();
+    const int x = (SCREEN_WIDTH - width) / 2;
+    const int y = (SCREEN_HEIGHT - height) / 2;
+    display.drawRect(x, y, width, height, SSD1306_WHITE);
+    display.display();
+#elif SCREEN_SAVER == SS_BARS
+    static int lastBars = 0;
+    const int numBars = 4;
+    int bars = (numBars + 1) * elapsedFraction;
+    if (!forceRedraw && bars == lastBars) {
+        return;
+    }
+    lastBars = bars;
+    
+    if (forceRedraw) {
+        display.clearDisplay();
+    }
+    display.fillRect(0, SCREEN_HEIGHT / numBars * (bars), SCREEN_WIDTH, SCREEN_HEIGHT / numBars, SSD1306_INVERSE);
+    display.display();
+#endif
 }
 
 inline int sign(int x) {
