b927ab28a9ca5cc95a3fcb3c3cb8abf28205882d
Quinn Freedman <quinnfreedman@gmail.com>
2020-04-07 00:47:10 -0700

    comments

--- RNG/RNG.ino
+++ RNG/RNG.ino
@@ -2,8 +2,9 @@
 #include <SPI.h>
 #include <Tlc5940.h>
 
-const int NUM_LEDS = 7;
-
+/*
+ * Pins
+ */
 const uint16_t RANDOMNESS_POT_PIN = A6;
 const uint16_t DAC_CS_PIN = 8;
 const uint16_t THRESHOLD_POT_PIN = A7;
@@ -13,20 +14,28 @@ const uint16_t RECORD_SWITCH_PIN = 5;
 const uint16_t GATE_TRIG_SWITCH_PIN = 4;
 const uint16_t CLOCK_IN_PIN = 2;
 
+/*
+ * Configuration
+ */
+const int NUM_LEDS = 7;
 const uint16_t LED_OFFSET = 3;
 const int TRIG_LENGTH_MS = 100;
 const int MAX_BUFFER_SIZE = 32;
 const bool YOYO = false;
 
-int bufferSize = 8;
-uint16_t buffer[MAX_BUFFER_SIZE];
-
 // The max value for numbers in the buffer (given
 // by the max value writable to the external DAC)
 const uint16_t MAX_VALUE = pow(2, 12) - 1;
 
-const int MAX_READ_VALUE = 1023; // Max value from an analogRead()
+// Max value from an analogRead()
+const int MAX_READ_VALUE = 1023;
 
+/*
+ * Global variables
+ */
+
+int bufferSize = 8;
+uint16_t buffer[MAX_BUFFER_SIZE];
 
 uint16_t randomness = 0;
 uint16_t threshold = 0;
@@ -35,26 +44,26 @@ uint32_t lastStepTime = 0;
 bool reverse = false;
 
 void setup() {
+    // setup pins
     pinMode(RANDOMNESS_POT_PIN, INPUT);
     pinMode(THRESHOLD_POT_PIN, INPUT);
     pinMode(DAC_CS_PIN, OUTPUT);
     pinMode(GATE_PIN_A, OUTPUT);
     pinMode(GATE_PIN_B, OUTPUT);
-
     pinMode(GATE_TRIG_SWITCH_PIN, INPUT_PULLUP);
     pinMode(RECORD_SWITCH_PIN, INPUT_PULLUP);
-
     pinMode(CLOCK_IN_PIN, INPUT);
 
     digitalWrite(DAC_CS_PIN, HIGH);
 
     attachInterrupt(digitalPinToInterrupt(CLOCK_IN_PIN), stepInterruptWrapper, CHANGE);
 
+    // Initialize 
     for (int i = 0; i < MAX_BUFFER_SIZE; i++) {
         buffer[i] = random(MAX_VALUE);
     }
 
-    Serial.begin(9600);
+    /* Serial.begin(9600); */
     
     Tlc.init();
     Tlc.clear();
@@ -71,6 +80,12 @@ void setup() {
     step();
 }
 
+/**
+ * The loop function runs continuously. It just does preparation stuff like
+ * always reading from the potentiometers so that when we get a signal from
+ * the clock we already have up-to-date values for those things so we don't
+ * have to waste time checking them.
+ */
 void loop() {
     randomness = analogRead(RANDOMNESS_POT_PIN);
     threshold = analogRead(THRESHOLD_POT_PIN) * (MAX_VALUE / MAX_READ_VALUE);
@@ -86,6 +101,12 @@ void loop() {
     }
 }
 
+/**
+ * Little wrapper function to call the `step()` function on a CHANGE
+ * interrupt from the clock pin. Maybe we should just bind step directly
+ * to a RISE trigger but I have gotten less false positives with this
+ * method
+ */
 void stepInterruptWrapper() {
     static int lastValue = digitalRead(CLOCK_IN_PIN);
     const int value = digitalRead(CLOCK_IN_PIN);
@@ -95,17 +116,23 @@ void stepInterruptWrapper() {
     lastValue = value;
 }
 
+/**
+ * This function is called via an interrupt whenever the module gets a clock pulse.
+ * It does all the core work of stepping through the ringbuffer, doing the random
+ * stuff, and setting the LEDs.
+ */
 void step() {
     static uint16_t ptr = 0;
 
-    Serial.println("step");
-
+    // maybe randomize the value at the cursor (ptr)
     if (random(MAX_READ_VALUE) < randomness) {
         buffer[ptr] = random(MAX_VALUE);
     }
     
+    // Write the value to our DAC chip to output the main analog value
     MCP4922_write(DAC_CS_PIN, 0, buffer[ptr]);
 
+    // handle the two gate outputs.
     bool gate;
     if (threshold <= 5) {
         gate = true;
@@ -114,16 +141,17 @@ void step() {
     } else {
         gate = buffer[ptr] > threshold;
     }
-    
     digitalWrite(GATE_PIN_A, gate ? HIGH : LOW);
     digitalWrite(GATE_PIN_B, gate ? LOW : HIGH);
 
+    // handle LEDs via TLC5940 driver
     for (uint16_t led = 0; led < NUM_LEDS; led++) {
         uint16_t index = (ptr + led - LED_OFFSET) % bufferSize;
         Tlc.set(led, buffer[index]);
     }
     Tlc.update();
 
+    // Update the ptr/cursor to point to the next value in the buffer
     if (!YOYO) {
         ptr = (ptr + 1) % bufferSize;
         reverse = false;
@@ -142,6 +170,8 @@ void step() {
             }
         }
     }
+
+    // remember when we were here so we know when to turn off any trigger pulses
     lastStepTime = lastRecordedTime;
 }
 
