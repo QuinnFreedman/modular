c6111e5517db01fcc8ed51887d3d16c1dea7312a
Quinn Freedman <quinnfreedman@gmail.com>
2020-08-25 14:14:02 -0700

    final touches

--- Simplex/Simplex.ino
+++ Simplex/Simplex.ino
@@ -10,9 +10,9 @@ extern "C" {
  */
 
 // The range of the speed or "frequency" of the noise.
-// Measured in terms of microsecconds per step.
+// Measured in terms of steps per microseccond.
 // 0.000001 (1e-6) is something like 1Hz.
-const double MAX_SPEED = 0.000008;//0.001;
+const double MAX_SPEED = 0.000008;
 const double MIN_SPEED = 0;
 
 // The range of the texture. Texture is just the coefficient applied to each
@@ -28,7 +28,7 @@ const uint16_t NUM_OCTAVES = 4;
 // than octave 1.
 const uint16_t OCTAVE_STEP = 4;
 
-// How long between when the CV, POTS are sampled. A lower value means 
+// How long between when the CV & pots are sampled. A lower value means 
 // more responsive controls but possibly a less smooth output.
 const uint16_t CV_SAMPLING_FREQUENCY = 5;
 
@@ -42,11 +42,15 @@ const uint16_t TRANSISTOR_5V_VALUE = 1017;
 // see: https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
 const uint16_t ANALOG_READ_MAX_VALUE = 1023;
 
+// A0 = top pot
+// A1 = bottom pot
+// A2 = middle pot
+
 //Pins
 const uint16_t CHIP_SELECT_PIN = 8;
 const uint16_t SPEED_POT_PIN = A0;
-const uint16_t TEXTURE_POT_PIN = A1;
-const uint16_t ATTENUATION_POT_PIN = A2;
+const uint16_t TEXTURE_POT_PIN = A2;
+const uint16_t ATTENUATION_POT_PIN = A3;
 const uint16_t SPEED_CV_PIN = A4;
 const uint16_t TEXTURE_CV_PIN = A3;
 
@@ -54,12 +58,9 @@ const uint16_t TEXTURE_CV_PIN = A3;
  * End config
  */
 
-const uint8_t SEEDS[NUM_OCTAVES] = {
-    random() * 256,
-    random() * 256,
-    random() * 256,
-    random() * 256,
-};
+const uint8_t NUM_CHANNELS = 2;
+
+uint8_t SEEDS[NUM_CHANNELS][NUM_OCTAVES];
 
 void setup() {
     pinMode(CHIP_SELECT_PIN, OUTPUT);
@@ -74,8 +75,9 @@ void setup() {
     SPI.setBitOrder(MSBFIRST);
     SPI.setDataMode(SPI_MODE0);
 
-    Serial.begin(9600);
-    
+    //Serial.begin(9600);
+
+    /*
     for (int i = 0; i < 5; i++) {
         MCP4922_write(CHIP_SELECT_PIN, 0, 1);
         MCP4922_write(CHIP_SELECT_PIN, 1, 1);
@@ -83,6 +85,13 @@ void setup() {
         MCP4922_write(CHIP_SELECT_PIN, 0, 0);
         MCP4922_write(CHIP_SELECT_PIN, 1, 1);
         delay(300);
+    }
+    */
+
+    for (int i = 0; i < NUM_CHANNELS; i++) {
+        for (int j = 0; j < NUM_OCTAVES; j++) {
+            SEEDS[i][j] = random(0, 256);
+        }
     }
 }
 
@@ -94,6 +103,7 @@ void loop() {
     static double texture = 0;
     static double speed = 0;
     static double amplitude = 0;
+    static uint16_t channel = 0;
 
     static double perlinTime = 0;
     static uint32_t loopCount = 0;
@@ -106,21 +116,25 @@ void loop() {
         perlinTime -= 256;
     }
 
-    for (int i = 0; i < 2; i++) {
+    {
         double noise = 0;
         double maxValue = 0;
         for (uint8_t i = 0; i < NUM_OCTAVES; i++) {
             uint8_t oct = i * OCTAVE_STEP;
             double decayValue = pow(texture, oct);
-            double x = SEEDS[i] + perlinTime * (oct + 1);
+            double x = SEEDS[channel][i] + perlinTime * (oct + 1);
             double randomValue = noise1d(x);
             noise += randomValue * decayValue;
             maxValue += decayValue;
         }
         noise /= maxValue;
-        noise *= amplitude;
-        MCP4922_write(CHIP_SELECT_PIN, i, (noise + 1) / 2);
+        if (channel == 0) {
+            noise *= amplitude;
+        }
+        MCP4922_write(CHIP_SELECT_PIN, channel, (noise + 1) / 2);
     }
+        
+    channel = (channel + 1) % NUM_CHANNELS;
 
     const uint16_t NUM_CV_CHANNELS = 5;
 
