e6093a628123f22d2f768b8c99e6561d0f077564
Quinn Freedman <quinnfreedman@gmail.com>
2021-01-11 20:31:00 -0800

    updated clock to use AVR PORTs

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -2,76 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-/*
- * Pins (for Arduino Nano)
- * Change if you want a different layout or a different board.
- * Note: this code is expecting ENCODER_PIN_A to be an external interrupt
- *       pin, ENCODER_BUTTON_PIN and PAUSE_BUTTON_PIN to be in PCINT2_vect,
- *       and CLOCK_INPUT to be in PCINT0_vect. Search the Arduino
- *       documentation if you're not sure about your board.
- */
-const uint8_t ENCODER_PIN_A = 3;
-const uint8_t ENCODER_PIN_B = 4;
-const uint8_t ENCODER_BUTTON_PIN = 5;
-const uint8_t CLOCK_INPUT_PIN = 8;
-const uint8_t PAUSED_LED_PIN = 7;
-const uint8_t PAUSE_BUTTON_PIN = 6;
-
-const uint8_t NUM_OUTPUTS = 8;
-const PROGMEM uint8_t OUTPUT_PINS[NUM_OUTPUTS] = {A3, 9, A2, 10, A1, 11,  A0, 12};
-
-/*
- * Configuration
- * Change the following values to change how the module works.
- */
- 
-// Number of (micro)seconds before the screen goes to sleep
-const uint32_t SLEEP_TIMEOUT_MICROS = 8 * 1000000;
-
-// How long you have to hold down the rotary button to count as a long press
-const uint32_t LONG_PRESS_TIME_MICROS = .5 * 1000000;
-
-// Symbol used for clock speeds that are SLOWER than base
-// Use 246 for the ASCII division symbol
-const char CLOCK_DIVISION_SYMBOL = '/';
-// Symbol used for clock speeds that are FASTER than base
-const char CLOCK_MULTIPLE_SYMBOL = 'x';
-// Character used for the menu cursor when edititng channel properties
-const char SUBMENU_CURSOR_SYMBOL = '>';
-
-//The initial values of each clock channel. Negative = division
-const PROGMEM int8_t DEFAULT_CLOCK_VALUES[NUM_OUTPUTS] = {1, -2, -4, -8, 2, 4, 8, 16};
-
-// The initial BPM value
-const uint8_t DEFAULT_BPM = 70;
-
-#define SWING_ENABLED         true
-#define PHASE_SHIFT_ENABLED   true
-#define PAUSE_BUTTON_ENABLED  false
-#define TAP_TEMPO_ENABLED     false
-#define CACHE_OUTPUTS_ENABLED true
-
-#if TAP_TEMPO_ENABLED
-// Min & max input clock deltas (micros) for running in follow mode.
-const uint32_t CLOCK_INPUT_TIME_MAX = 60000000 / 35;
-const uint32_t CLOCK_INPUT_TIME_MIN = 200000;
-#endif
-
-#if SWING_ENABLED 
-// What is the maximum value that can be set for the swing of each clock
-const int8_t MAX_SWING_ENABLED = 75;
-// What is the maximum value that can be set for the swing of each clock
-// (i.e. can swing be negative?)
-const int8_t MIN_SWING_ENABLED = 0;
-// Swing is divided by this number to get a fraction from -1 to 1.
-const float SWING_ENABLED_SCALE = 100;
-#endif
-
-#if PAUSE_BUTTON_ENABLED
-// If true, the LED pin will be set HIGH when paused.
-// Otherwise, will be set HIGH when not paused
-const bool GLOW_ON_PAUSED = true;
-#endif
+#include "config.h"
 
 #if SWING_ENABLED && PHASE_SHIFT_ENABLED
 #define MENU_SIZE 3
@@ -80,37 +11,6 @@ const bool GLOW_ON_PAUSED = true;
 #else
 #define MENU_SIZE 1
 #endif
-
-/*
- * OLED screen settings. See Adafruit_SSD1306 for more info.
- */
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_I2C_ADDRESS 0x3C // Change to correct address for your screen
-
-/*
- * Screen saver mode. After SLEEP_TIMEOUT_MICROS microseconds since the last
- * user input, the screen will go to one of these modes.
- *
- * Set SCREEN_SAVER to be equal to whatever mode you want to use.
- *
- * Note - drawing to the screen takes time (I2C is really slow)
- *        so setting a fast-moving screen saver may cause fast
- *        subdivisions to be a little out of sync. SS_NONE, SS_BLACK,
- *        and SS_BPM all don't require any redraws after the first one
- *        so they should be fine.
- */
-#define SS_NONE  0 // No screensaver -- stay on the current menu
-#define SS_BLACK 1 // Turn the screen off
-#define SS_BPM   2 // Return to the BPM-select screen
-#define SS_PULSE 3 // Show a pulsing 1-beat animation
-#define SS_BARS  4 // Show a 4-beat scroll animation
-#define SCREEN_SAVER SS_BARS 
-
-/*
- * End of Configuration
- */
  
 typedef enum : uint8_t {NAVIGATE, EDIT_FAST, SUBMENU, SUBMENU_EDIT, SLEEP} MenuMode;
 typedef enum : uint8_t {MULTIPLY, DIVIDE} ClockMode;
@@ -146,7 +46,35 @@ uint32_t lastRecordedTime;
 #if CACHE_OUTPUTS_ENABLED
 //Cache the values that we output to each clock channel so we don't have to
 //call digitalWrite() as often, which takes time
-uint8_t outputCache[NUM_OUTPUTS] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+//uint8_t outputCache[NUM_OUTPUTS] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+#if !defined(NUM_OUTPUTS) || NUM_OUTPUTS > 64 || NUM_OUTPUTS <= 0
+    static_assert(false, "NUM_OUTPUTS must be between 1 and 64");
+#elif NUM_OUTPUTS > 32
+    #define cache_t uint64_t 
+#elif NUM_OUTPUTS > 16
+    #define cache_t uint32_t 
+#elif NUM_OUTPUTS > 8
+    #define cache_t uint16_t 
+#else
+    #define cache_t uint8_t
+#endif
+cache_t outputCache = 0;
+#endif
+
+#if defined(__AVR__) && PORTS_ENABLED
+static_assert(
+    OUTPUT_PINS[0] == A3 &&
+    OUTPUT_PINS[1] == 9 &&
+    OUTPUT_PINS[2] == A2 &&
+    OUTPUT_PINS[3] == 10 &&
+    OUTPUT_PINS[4] == A1 &&
+    OUTPUT_PINS[5] == 11 &&
+    OUTPUT_PINS[6] == A0 &&
+    OUTPUT_PINS[7] == 12,
+    "Port code is writeen assuming the default output pins. If you change them, you will have to change the code (or don't use ports).");
+    constexpr uint8_t PORTB_MASK = 0b11100001;
+    constexpr uint8_t PORTC_MASK = 0b11110000;
+    #define USE_PORTS
 #endif
 
 #if PAUSE_BUTTON_ENABLED
@@ -269,21 +197,42 @@ void loop() {
         /*
          * Send signals to output pins
          */
+        #ifdef USE_PORTS
+        uint8_t outputAllPins = 0;
+        #endif
         for (int i = 0; i < NUM_OUTPUTS; i++) {
             const Clock clock = state.clocks[i];
 
             float elapsed = getElapsedFractionForClock(&clock, elapsedFraction, numBeats);
             
-            const uint8_t output = elapsed < (float) clock.pulseWidth / (float) 100 ? HIGH : LOW;
-            #if CACHE_OUTPUTS_ENABLED
-            if (output != outputCache[i]) {
-                outputCache[i] = output;
+            const bool output = elapsed < (float) clock.pulseWidth / (float) 100;
+            #ifdef USE_PORTS
+            bitWrite(outputAllPins, i, output);
+            #elif CACHE_OUTPUTS_ENABLED
+            if (output != bitRead(outputCache, i)) {
+                bitWrite(outputCache, i, output);
                 digitalWrite(pgm_read_byte(&OUTPUT_PINS[i]), output);
             }
             #else
                 digitalWrite(pgm_read_byte(&OUTPUT_PINS[i]), output);
             #endif
         }
+        #ifdef USE_PORTS
+        {
+            uint8_t B_OUT = 0;
+            B_OUT |= bitRead(outputAllPins, 1) << 1;
+            B_OUT |= bitRead(outputAllPins, 3) << 2;
+            B_OUT |= bitRead(outputAllPins, 5) << 3;
+            B_OUT |= bitRead(outputAllPins, 7) << 4;
+            uint8_t C_OUT = 0;
+            C_OUT |= bitRead(outputAllPins, 6) << 0;
+            C_OUT |= bitRead(outputAllPins, 4) << 1;
+            C_OUT |= bitRead(outputAllPins, 2) << 2;
+            C_OUT |= bitRead(outputAllPins, 0) << 3;
+            PORTB = (PORTB & PORTB_MASK) | B_OUT; 
+            PORTC = (PORTC & PORTC_MASK) | C_OUT; 
+        }
+        #endif
     } else {
         elapsedFraction = ((float) pausedTime) / ((float) beatMicros);
     }
@@ -403,7 +352,7 @@ inline float getElapsedFractionForClock(const Clock* clock,
     // Handle swing
     // 
     if (clock->swing && numClockCyclesElapsed % 2 == 1) {
-        const float swingFraction = clock->swing / SWING_ENABLED_SCALE;
+        const float swingFraction = clock->swing / SWING_SCALE;
         
         elapsedFractionOfClockCycle -= swingFraction;
         if (elapsedFractionOfClockCycle < 0) {
@@ -639,7 +588,7 @@ inline void onKnobTurned(int direction, int counter) {
         #endif
         #if SWING_ENABLED
         default: {
-            addMaxMin(&clock->swing, direction, MIN_SWING_ENABLED, MAX_SWING_ENABLED);
+            addMaxMin(&clock->swing, direction, MIN_SWING, MAX_SWING);
         } break;
         #endif
         }
