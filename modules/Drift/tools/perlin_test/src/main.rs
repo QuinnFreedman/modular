ab63732b2bcf3fc5a650bab38b8e5f3ab15aa715
Quinn Freedman <quinnfreedman@gmail.com>
2024-08-29 21:24:23 -0700

    [WIP] optimizing perlin

--- modules/Drift/tools/perlin_test/src/main.rs
+++ modules/Drift/tools/perlin_test/src/main.rs
@@ -1,25 +1,65 @@
-use fixed::types::I16F16;
+use fixed::{
+    traits::{Fixed, FromFixed},
+    types::{I0F16, I16F16, I1F15, U0F16},
+};
 use rand::prelude::*;
 use textplots::{Chart, Plot, Shape};
 
 fn main() {
     show_perlin();
+    // let mut values = vec![];
+    // for i in (0..u16::MAX).step_by(1 << 10) {
+    //     let x = i as f32 / u16::MAX as f32;
+    //     values.push((x, fade(I16F16::from_num(x)).to_num::<f32>()));
+    // }
+    // println!("{:?}", values);
+    // return Chart::new(120, 60, 0.0, 1.0)
+    //     .lineplot(&Shape::Lines(&values))
+    //     .nice();
+    // println!("{}", fade(I16F16::from_bits(u16::MAX as i32)))
 }
 
-fn fade(t: I16F16) -> I16F16 {
+// TODO convert fade to I1F15 -- cleaner code from less converting + can take advantage of expansion optimization
+fn fade(t: U0F16) -> U0F16 {
+    const SIX: U0F16 = U0F16::from_bits(6 << 12);
+    const FIFTEEN: U0F16 = U0F16::from_bits(15 << 12);
+    const TEN: U0F16 = U0F16::from_bits(10 << 12);
+    debug_assert!(t < 1);
+    debug_assert!(t >= 0);
+    let t_3 = t * t * t;
+    let t_4 = t_3 * t;
+    let t_5 = t_4 * t;
+    // println!("t_3: {t_3}, t_4: {t_4}, t_5: {t_5}");
+    let result = (TEN * t_3).saturating_add(SIX * t_5) - FIFTEEN * t_4;
+    // println!("result: {result} | {}", result.to_bits());
+    let result_bits = u16::min(result.to_bits(), 4095);
+    U0F16::from_bits(result_bits << 4)
+}
+
+fn fade1(t: I16F16) -> I16F16 {
     const SIX: I16F16 = I16F16::from_bits(6 << 16);
     const FIFTEEN: I16F16 = I16F16::from_bits(15 << 16);
     const TEN: I16F16 = I16F16::from_bits(10 << 16);
-    t * t * t * (t * (t * SIX - FIFTEEN) + TEN)
+    // t * t * t * (t * (t * SIX - FIFTEEN) + TEN)
+    // println!("{t} | {}", t.to_bits());
+    debug_assert!(t < 1);
+    debug_assert!(t >= 0);
+    let t_3 = t * t * t;
+    let t_4 = t_3 * t;
+    let t_5 = t_4 * t;
+    (TEN * t_3).saturating_add(SIX * t_5) - FIFTEEN * t_4
 }
 
-fn lerp(t: f32, a: f32, b: f32) -> f32 {
-    a + t * (b - a)
-}
-
-fn grad(hash: u8, x: I16F16) -> I16F16 {
+fn grad(hash: u8, x: I1F15) -> I1F15 {
+    debug_assert!(x < 1);
+    debug_assert!(x > -1);
+    // let x_1_15 = I1F15::from_bits(((x.to_bits() as u16) >> 1) as i16);
     let h = hash & 15;
-    let grad = I16F16::from_num(1 + (h & 7));
+    let grad_int = 1 + (h & 7);
+    // TODO
+    // let grad = I16F16::from_bits(((grad_int as u16) << 14) as i32);
+    // let grad = I16F16::from_num(grad_int) / 8;
+    let grad = I1F15::from_bits(((grad_int as u16) << 11) as i16);
     if (h & 8) != 0 {
         -grad * x
     } else {
@@ -53,14 +93,20 @@ fn grad(hash: u8, x: I16F16) -> I16F16 {
 fn perlin1d(_x: f32, permutation: &[u8]) -> f32 {
     let x = I16F16::from_num(_x);
     let xi = x.int().to_num::<i32>() & 255;
-    let xf = x.frac();
+    let xf = U0F16::from_bits(x.frac().to_bits() as u16);
 
     let u = fade(xf);
+    let u_i16 = I1F15::from_bits((u.to_bits() >> 1) as i16);
 
     let a = permutation[xi as usize];
     let b = permutation[(xi + 1) as usize & 255];
 
-    u.lerp(grad(a, xf), grad(b, xf - I16F16::from_num(1)))
+    let xf_signed = I1F15::from_bits(((xf.to_bits() as u16) >> 1) as i16);
+    u_i16
+        .lerp(
+            grad(a, xf_signed),
+            grad(b, xf_signed - I1F15::from_bits(0x7FFF)),
+        )
         .to_num()
 }
 
