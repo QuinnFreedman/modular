51e0c7c02bb8591d68e6e95fb3f0079bfd15ad4b
Quinn Freedman <quinnfreedman@gmail.com>
2024-09-19 11:27:46 -0700

    Add boost module

---
+++ modules/Boost/Faceplate/make_boost_faceplate.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("..")
+sys.path.append("../..")
+sys.path.append("../../..")
+
+from faceplate_maker import *
+
+module = Module.from_cli(4, global_y_offset=12, title="Boost")
+
+module.add(Potentiometer(inches(-.1), inches(.8), label="Drive", color=PotColor.RED))
+module.add(Potentiometer(inches(-.1), inches(1.7), label="Tone", color=PotColor.WHITE))
+
+module.add(JackSocketCentered(inches(0), inches(2.8), "In", False, rotation=2))
+module.add(JackSocketCentered(inches(0), inches(2.8 + 0.7), "Out", True, rotation=2))
+
+
+module.save()
---
+++ modules/Boost/README.md
@@ -0,0 +1,12 @@
+<div class="fm-readme-container">
+
+<img class="fm-readme-module-image" src="docs/images/output_faceplate.svg" />
+
+<div class="fm-readme-text">
+
+<h1>Boost</h1>
+
+<P>⚠️ <b>Work in progress.</b> This module is still in development and may not be in a working state. It will be added to the modules list as soon as it is completed.</P>
+
+</div>
+</div>
