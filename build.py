c110f1135bdadd9e5d0a5db28d0eea50c496441b
Quinn Freedman <quinnfreedman@gmail.com>
2024-09-22 00:10:01 -0700

    Fix boost format

--- build.py
+++ build.py
@@ -329,5 +329,5 @@ if __name__ == "__main__":
     build("OffsetAtten", output_dir, [("front", "B1"), ("back", "B2")])
     build("Envelope", output_dir, [("front", "B1"), ("back", "B2")])
     build("Drift", output_dir, [("front", "B1"), ("back", "B2")])
-    build("Boost", output_dir, [("front", "B1"), ("back", "B2")])
+    build("Boost", output_dir)
 
