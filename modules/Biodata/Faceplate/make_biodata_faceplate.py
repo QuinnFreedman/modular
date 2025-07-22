362b003bfa157d27c81039e19756a32e7a0c3059
Quinn Freedman <quinnfreedman@gmail.com>
2025-07-22 13:21:39 -0700

    Working on biodata layout and PCB

--- modules/Biodata/Faceplate/make_biodata_faceplate.py
+++ modules/Biodata/Faceplate/make_biodata_faceplate.py
@@ -13,22 +13,22 @@ module.add(JackSocketCentered(-inches(.6), inches(.3), "Probe", False, rotation=
 module.add(Potentiometer(inches(.4), inches(.6), "Sensitivity", False, color=PotColor.BLUE))
 module.add(Potentiometer(-inches(.6), inches(1.3), "Density", False, color=PotColor.GREEN))
 
-module.add(SmallPotentiometer(-inches(.4), inches(2.0), "Slew"))
-module.add(SmallPotentiometer(-inches(.65), inches(2.7), "Spread"))
+module.add(SmallPotentiometer(-inches(.35), inches(2.2), "Slew"))
+module.add(SmallPotentiometer(-inches(.85), inches(2.7), "Spread"))
 
 module.add(LED(-inches(.75), inches(3.05), color="blue"))
 module.add(JackSocketCentered(-inches(.25), inches(3.1), "Sample", False, rotation=2))
-module.add(JackSocketCentered(-inches(.25), inches(3.7), "CV", True, rotation=2))
+module.add(JackSocketCentered(-inches(.375), inches(3.7), "CV", True, rotation=2))
 module.add(JackSocketCentered(-inches(.75), inches(3.7), "Gate", True, rotation=2))
 
-module.add(SmallPotentiometer(inches(.2), inches(2.0), "Slew"))
-module.add(SmallPotentiometer(inches(.45), inches(2.7), "Spread"))
+module.add(SmallPotentiometer(inches(.65), inches(2.2), "Slew"))
+module.add(SmallPotentiometer(inches(.15), inches(2.7), "Spread"))
 
-module.add(LED(inches(.75), inches(3.05), color="green"))
-module.add(JackSocketCentered(inches(.25), inches(3.1), "Sample", False, rotation=2))
-module.add(JackSocketCentered(inches(.25), inches(3.7), "CV", True, rotation=2))
-module.add(JackSocketCentered(inches(.75), inches(3.7), "Gate", True, rotation=2))
+module.add(LED(inches(.25), inches(3.05), color="green"))
+module.add(JackSocketCentered(inches(.75), inches(3.1), "Sample", False, rotation=2))
+module.add(JackSocketCentered(inches(.8), inches(3.7), "CV", True, rotation=2))
+module.add(JackSocketCentered(inches(.4), inches(3.7), "Gate", True, rotation=2))
 
-module.add(JackSocketCentered(inches(0), inches(2.4), "Raw", True, rotation=2))
+module.add(JackSocketCentered(inches(0), inches(3.7), "Raw", True, rotation=2))
 
 module.save()
