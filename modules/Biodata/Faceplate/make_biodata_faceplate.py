048b4e5c5fffcbcf56b4aafe7775fe2c251f1bfa
Quinn Freedman <quinnfreedman@gmail.com>
2025-10-11 18:41:59 -0700

    Switch biodata to monophonic

--- modules/Biodata/Faceplate/make_biodata_faceplate.py
+++ modules/Biodata/Faceplate/make_biodata_faceplate.py
@@ -7,13 +7,11 @@ import math
 from faceplate_maker import *
 from svgwrite.path import Path
 
-module = Module.from_cli(12, global_y_offset=17, title="Biodata")
+module = Module.from_cli(6, global_y_offset=17, title="Biodata")
 
-left_col = -inches(.8)
-
-probe_x = left_col
+probe_x = inches(-.3)
 probe_y = inches(.1)
-module.add(JackSocketCentered(probe_x, probe_y, "Probe", False, rotation=1))
+module.add(JackSocketCentered(probe_x, probe_y, "Probe", False, rotation=3))
 # module.draw(lambda c: c.text("Probe", insert=(-probe_y, probe_x - inches(.25)), transform="rotate(-90)", text_anchor="middle"))
 
 path = Path(stroke="black", fill="none", stroke_width=.8)
@@ -23,27 +21,21 @@ path.push(f"A {r} {r} 0 1 0 {probe_x-r} {probe_y}")
 path.push(f"A {r} {r} 0 1 0 {probe_x+r} {probe_y}")
 module.draw(lambda _: path)
 
-module.add(Potentiometer(left_col - inches(.1), inches(1.2), "Sensitivity", False, color=PotColor.BLUE))
-module.add(Potentiometer(left_col - inches(.1), inches(2.15), "Density", False, color=PotColor.GREEN))
-module.add(JackSocketCentered(left_col, inches(2.75), "CV", True, rotation=2))
-module.add(JackSocketCentered(left_col, inches(3.4), "Raw", True, rotation=2))
+module.add(Potentiometer(-inches(.1), inches(1.3), "Sensitivity", False, color=PotColor.GREEN, style=PotStyle.SIFAM_LARGE))
+y = inches(2.3)
+module.add(SmallPotentiometer(-inches(.3)-inches(.1), y, "Density", False))
+module.add(SmallPotentiometer(inches(.3)-inches(.1), y, "Spread"))
 
-led_colors = ["blue", "green", "blue"]
 
-for i in range(3):
-    odd = True #i % 2 == 0
-    x = i * inches(.525) - inches(.15)
-    y = inches(0.4)
-    module.add(SmallPotentiometer(x - inches(.1), y, "Spread"))
-    y += inches(.9)
-    module.add(SmallPotentiometer(x - inches(.1), y, "Slew"))
-    y += inches(.3)
-    module.add(LED(x, y, color=led_colors[i]))
-    y += inches(.5)
-    module.add(JackSocketCentered(x, y, "Sample", False, rotation=2))
-    y += inches(.65)
-    module.add(JackSocketCentered(x, y, "CV", True, rotation=2))
-    y += inches(.65)
-    module.add(JackSocketCentered(x, y, "Gate", True, rotation=2))
+ 
+y = inches(2.75)
+module.add(JackSocketCentered(inches(-.4), y, "Trig", False, rotation=2))
+module.add(JackSocketCentered(inches(.4), y, "Gate", True, rotation=2))
+module.add(LED(0, y-inches(.05), color="green"))
+
+y += inches(.65)
+module.add(JackSocketCentered(inches(-.4), y, "Raw", True, rotation=2))
+module.add(JackSocketCentered(inches(.4), y, "V/Oct", True, rotation=2))
+module.add(JackSocketCentered(inches(0), y, "CV", True, rotation=2))
 
 module.save()
